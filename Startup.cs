863de3f501777c9bbdee1f061c4ebef929e541d0
Jon Elliott <jonathanelliott1247@gmail.com>
2019-09-08 17:03:01 -0500

    Setup file server middleware.

--- Startup.cs
+++ Startup.cs
@@ -39,10 +39,11 @@ namespace walkie_talkie_core
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseMvc();
+            app.UseFileServer();
         }
     }
 }
